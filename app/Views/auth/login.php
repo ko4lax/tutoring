8a93b3b46071593086f7cb9e0438d26e91baad88
Onyx <onyx@ko4lax.dev>
2026-04-28 01:47:39 +0700

    fix: baseURL updated to port 8081 (was 8080 where filebrowser runs)

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -458,7 +458,7 @@
     </div>
 
     <!-- Scripts -->
-    <script src="<?= https://code.jquery.com/jquery-3.7.0.min.js ?>"></script>
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
     <script>
         // Toggle Password Visibility
         function togglePassword() {
